f3a0cf91358b915e47c9708e648e19010c9a6ef4
Tierry Ray <tierry.ray@ifood.com.br>
2021-05-26 10:37:04 -0300

    feat(memo): using memo to avoid unnecessary renders on ProductItem

---
+++ components/ProductItem.tsx
@@ -0,0 +1,26 @@
+import { memo } from 'react';
+
+interface ProductItemProps {
+  product: {
+    id: number;
+    price: number;
+    title: string;
+  }
+}
+
+//  shallow compare -> comparação rasa
+
+//  {} === {} // false
+//  igualdade referencial
+
+function ProductItemComponent({ product }: ProductItemProps) {
+  return (
+    <div>
+      {product.title} - <strong>{product.price}</strong>
+    </div>
+  )
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+});
---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import { FormEvent, useState } from "react"
+
+import { SearchResults } from "../components/SearchResults";
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json();
+
+    setResults(data);
+  }
+
+  return (
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  )
+}
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * memo
+ * 1. Pure Functional Components (funções que dados os mesmos parâmetros, sempre retornam o mesmo resultado)
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
