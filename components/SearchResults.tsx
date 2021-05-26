bd62013bd2c4e28fed20d9bd1ebb0fe6a5a6e5ef
Tierry Ray <tierry.ray@ifood.com.br>
2021-05-26 11:57:43 -0300

    feat(search-results): add react virtualized on application

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from "./ProductItem"
-import { useMemo } from 'react';
 
 interface SearchResultsProps {
   results: Array<{
@@ -13,19 +14,29 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, onAddToWishlist, totalPrice }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist} 
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishlist={onAddToWishlist} 
-          />
-        )
-      })}
+      <List 
+        height={300}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
